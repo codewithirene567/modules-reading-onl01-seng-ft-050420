daa0f1ac664ae6128b7bc47178467d17a0c34c46
Irene Scott <irenejinscott@gmail.com>
2020-05-22 16:12:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  
+  include Dance #this is where we allow our classes Dancer and Kid to use all the methods of the included module as instance methods as instance methods
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance #I think this part brings in the dance module
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  extend MetaDancing
+end
